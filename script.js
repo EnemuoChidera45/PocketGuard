5ef5c126d614baff5bdb3fb560563292dd48ecda
divecash234 <158511593+divecash234@users.noreply.github.com>
2026-03-27 10:31:24 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+// Top action buttons
+document.getElementById("sendBtn").addEventListener("click", () => {
+  alert("Send button clicked!");
+});
+
+document.getElementById("addFundsBtn").addEventListener("click", () => {
+  alert("Add Funds button clicked!");
+});
+
+document.getElementById("requestBtn").addEventListener("click", () => {
+  alert("Request button clicked!");
+});
+
+document.getElementById("moreBtn").addEventListener("click", () => {
+  alert("More button clicked!");
+});
+
+// Bottom nav buttons
+document.getElementById("homeTab").addEventListener("click", () => {
+  alert("Home tab clicked!");
+});
+
+document.getElementById("walletTab").addEventListener("click", () => {
+  alert("Wallet tab clicked!");
+});
+
+document.getElementById("settingsTab").addEventListener("click", () => {
+  alert("Settings tab clicked!");
+});
